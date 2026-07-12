b50ccfd29cfe978e44224de1b175f7bf1d3ed8da
Fahd333-f <zxfahd217@gmail.com>
2026-07-13 00:30:46 +0300

    first commit

---
+++ ex1.c
@@ -0,0 +1,16 @@
+//lab1
+#include<stdio.h>
+int main(){
+    int num;
+
+    printf("enter the number : \n");
+    scanf("%d",&num);
+    if(num%2==0){
+        printf("the number was even : \n");
+    }else{
+        printf("the number was odd  : \n");
+
+    }
+    return 0;
+
+    }
---
+++ ex2.c
@@ -0,0 +1,14 @@
+// same like ex1 but with bitwise operators
+#include<stdio.h>
+int main(){
+
+    int num;
+    puts("enter the number : ");
+    scanf("%d",&num);
+    if(num & 1){
+        puts(" the number was odd  : ");
+
+    }else{
+        puts("the number was even : ");
+    }
+}
