562f63c1256c119fe157bf0d17677829b0525c75
Fahd333-f <zxfahd217@gmail.com>
2026-08-30 13:39:16 +0300

    done

--- Pointers/ex5.c
+++ Pointers/ex5.c
@@ -1,7 +1,53 @@
 #include <stdio.h>
-void print_string(char *str);
-
+#include <stdlib.h>
+#include <string.h>
 int main()
 {
+    int array = 0;
+    char arr[50] = {0};
+    printf("Enter the number of elemnts : \n");
+    scanf(" %d", &array);
+    int *p = (int *)calloc(array, sizeof(int));
+    if (p == NULL)
+    {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+    for (int i = 0; i < array; i++)
+    {
+        printf("Enter the number of p[%d] :  ", i);
+        scanf("%d", &p[i]);
+    }
 
+    for (int i = 0; i < array; i++)
+    {
+        printf("the elemnt if p[%d] : \t %d \n", i, p[i]);
+    }
+    printf("Do you to resize array : \n");
+    scanf("%s", arr);
+    if ((strcmp(arr, "yes") == 0))
+    {
+        printf("Enter the new size : \n");
+        scanf("%d", &array);
+
+        int *temp = (int *)realloc(p, array * sizeof(int));
+        if (temp == NULL)
+        {
+            printf("Reallocation failed!\n");
+        }
+        else
+        {
+
+            p = temp;
+            printf("Successfully resized.\n");
+        }
+    }
+    else
+    {
+        printf("You are welcome : \n");
+    }
+    free(p);
+    p = NULL;
+
+    return 0;
 }
